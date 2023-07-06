c5373776bc47c0854cf2861353b8cf6850929812
ung <test@abc.com>
2023-07-06 13:57:02 +0700

    map Success

--- lib/state/main_home_user.dart
+++ lib/state/main_home_user.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:monitorproduct/utility/app_controller.dart';
 import 'package:monitorproduct/utility/app_service.dart';
 import 'package:monitorproduct/widget/widget_signout.dart';
@@ -13,10 +14,19 @@ class MainHomeUser extends StatefulWidget {
 }
 
 class _MainHomeUserState extends State<MainHomeUser> {
+  var markers = <Marker>[];
+  AppController controller = Get.put(AppController());
+
   @override
   void initState() {
     super.initState();
-    AppService().processFindPosition(context: context);
+    AppService().processFindPosition(context: context).then((value) {
+      Marker userMarker = Marker(
+          markerId: const MarkerId('user'),
+          position: LatLng(controller.positions.last.latitude,
+              controller.positions.last.longitude), infoWindow: const InfoWindow(title: 'คุณอยู่ที่นี่'),);
+      markers.add(userMarker);
+    });
   }
 
   @override
@@ -24,16 +34,30 @@ class _MainHomeUserState extends State<MainHomeUser> {
     return Scaffold(
       appBar: AppBar(
         title: WidgetText(data: 'User'),
+        actions: [WidgetSignOut()],
       ),
-      floatingActionButton: WidgetSignOut(),
-      body: GetX(
-          init: AppController(),
-          builder: (AppController appController) {
-            print('## positions ---> ${appController.positions.length}');
-            return appController.positions.isEmpty
-                ? const SizedBox()
-                : WidgetText(data: 'data');
-          }),
+      body: LayoutBuilder(builder: (context, BoxConstraints boxConstraints) {
+        return GetX(
+            init: AppController(),
+            builder: (AppController appController) {
+              print('## positions ---> ${appController.positions.length}');
+              return appController.positions.isEmpty
+                  ? const SizedBox()
+                  : SizedBox(
+                      width: boxConstraints.maxWidth,
+                      height: boxConstraints.maxHeight,
+                      child: GoogleMap(
+                        initialCameraPosition: CameraPosition(
+                          target: LatLng(appController.positions.last.latitude,
+                              appController.positions.last.longitude),
+                          zoom: 16,
+                        ),
+                        onMapCreated: (controller) {},
+                        markers: markers.toSet(),
+                      ),
+                    );
+            });
+      }),
     );
   }
 }
